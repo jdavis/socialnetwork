e3b35a5d07e43bdbe53e7dc27d3042b16a827717
Josh Davis <josh@joshldavis.com>
2013-11-03 15:55:22 -0600

    Add media settings

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
+
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls.static import static
 
-urlpatterns = patterns('',
+
     url(r'^$', include('newsfeed.urls', namespace='newsfeed')),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
