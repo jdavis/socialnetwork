0193b170381b683c02008ec3e7f746c74fdf7be0
Josh Davis <josh@joshldavis.com>
2013-11-02 19:07:21 -0500

    Add django_extensions app

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -118,16 +118,18 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
+    # Django provided apps
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+
+    # Helper app that gives various features
+    'django_extensions',
+
+    # Custom app for tsn
     'newsfeed',
 )
 
