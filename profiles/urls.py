d0cbd52ba5e71196548d66650af9a6c9e594834d
Shawn Reisner <Shawn.Reisner@gmail.com>
2013-12-12 02:43:39 -0600

    Associated profile information with users.
    Created profile information for each user and connected them with a foreign
    key.  Began updating profile page with profile data.

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
+
+from profiles import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='profiles/index.html')),
+    url(r'^$', views.profiles, name='profiles'),
 )
--- profiles/views.py
+++ profiles/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render
+
+def profiles(request):
+    # NOTE:  Why is there a list of profiles?
+    profile = request.user.userprofile_set.all()[0]
+    return render(request, 'profiles/index.html', {"profile": profile})
