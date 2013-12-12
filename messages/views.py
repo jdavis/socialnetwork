28f7d0c4b4f0b5a1b9bbf43bd55cb6916ef52f36
Josh Davis <josh@joshldavis.com>
2013-12-11 20:47:36 -0600

    Add dummy data

--- messages/views.py
+++ messages/views.py
@@ -1,9 +1,54 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
+from datetime import datetime
+
+from django.shortcuts import render
 
 
 def messages(request):
     context = {}
+    context['messages'] = dummy_data
 
     return render(request, 'messages/index.html', context)
+
+dummy_data = {
+    'Shredder': [
+        {
+            'sent': False,
+            'content': 'Hey there Donatello. Want to go out and get some pizza?',
+            'viewed': True,
+            'modified': datetime.now(),
+        },
+        {
+            'sent': True,
+            'content': 'Sure. What time were you thinking? I\'m about to attack Shredder now',
+            'viewed': True,
+            'modified': datetime.now(),
+        },
+        {
+            'sent': False,
+            'content': 'How about at 4pm?',
+            'viewed': True,
+            'modified': datetime.now(),
+        },
+    ],
+
+    'Leonardo': [
+        {
+            'sent': False,
+            'content': 'Hey there Donatello. Want to go out and get some pizza?',
+            'viewed': True,
+            'modified': datetime.now(),
+        },
+        {
+            'sent': True,
+            'content': 'Sure. What time were you thinking? I\'m about to attack Shredder now',
+            'viewed': True,
+            'modified': datetime.now(),
+        },
+        {
+            'sent': False,
+            'content': 'How about at 4pm?',
+            'viewed': True,
+            'modified': datetime.now(),
+        },
+    ]
+}
