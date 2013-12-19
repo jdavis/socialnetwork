cf4e96b0afdd964f8980db77c25505a87ff7d38f
Josh Davis <josh@joshldavis.com>
2013-12-19 07:13:56 -0600

    Add MessageForm

--- messages/models.py
+++ messages/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.forms import ModelForm
 
 
 class Message(models.Model):
     content = models.TextField(blank=False)
 
     viewed = models.BooleanField(default=False)
-    sender = models.ForeignKey('profiles.UserProfile', related_name='messages_to')
-    recepient = models.ForeignKey('profiles.UserProfile', related_name='messages_from')
+    sender = models.ForeignKey('profiles.UserProfile',
+                               related_name='messages_to')
+    recepient = models.ForeignKey('profiles.UserProfile',
+                                  related_name='messages_from')
 
     created = models.DateTimeField(auto_now=True)
     modified = models.DateTimeField(auto_now_add=True)
+
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['content']
