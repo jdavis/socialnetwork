db487abbc81111c87a3d120eece54f388144c259
Josh Davis <josh@joshldavis.com>
2013-12-19 07:16:40 -0600

    Starting to add in message sending
    Work in progress.

--- messages/views.py
+++ messages/views.py
@@ -2,12 +2,31 @@ from datetime import datetime
 
 from django.shortcuts import render
 
+from messages.models import Message, MessageForm
+
 
 def messages(request):
     context = {}
     context['messages'] = dummy_data
 
+    message = Message()
+    if request.method == 'POST':
+        form = MessageForm(request.POST, instance=message)
+        form.sender = request.user
+        form.save()
+    else:
+        form = MessageForm(request.POST, instance=message)
+
+    context['message_form'] = form
+    context['current_thread'] = 'Shredder'
+    context['threads'] = dummy_data.keys()
+    context['messages'] = dummy_data['Shredder']
+
     return render(request, 'messages/index.html', context)
+
+def post(request):
+    pass
+
 
 dummy_data = {
     'Shredder': [
