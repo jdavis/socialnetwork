a501479f2173ed382e7d52bf11b7a0491bdb1490
Josh Davis <josh@joshldavis.com>
2013-12-20 01:03:29 -0600

    Change get to post

--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,29 +1,36 @@
+from django.shortcuts import render, redirect
 from django.views import generic
-from models import Status, LikesRelationship, StatusRelationship
+
+from models import Status, StatusForm, LikesRelationship, StatusRelationship
+
 import friends.views
+
 
 class IndexView(generic.TemplateView):
     template_name = 'newsfeed/index.html'
     context_object_name = 'latest_newsfeed'
 
     def get(self, request, *args, **kwargs):
-        if "statusUpdate" in request.GET:
-            status = Status(content=request.GET["statusUpdate"])
-            status.save()
-            relationship = StatusRelationship(user=request.user,
-                                              status=status)
-            relationship.save()
-
         friend_list = friends.views.get_friends(request.user)
         friend_list.append(request.user)
+
         # Get statuses that friends have posted.
-        s_relationships = []
-        s = StatusRelationship.objects.order_by('-created')
-        for r in s.all():
-            if r.user in friend_list:
-                if len(s_relationships) >= 25:
-                    break
-                else:
-                    s_relationships.append(r)
-        kwargs["statuses"] = s_relationships
+        statuses = Status.objects.order_by('-created')[:25]
+        kwargs["statuses"] = statuses
+
         return super(IndexView, self).get(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        s = Status()
+        s.owner = request.user.get_profile()
+
+        status = StatusForm(request.POST, instance=s)
+        status.save()
+
+        #relationship = StatusRelationship(user=request.user,
+                                          #status=status)
+        #relationship.save()
+
+        return super(IndexView, self).get(self, request, *args, **kwargs)
+
+
