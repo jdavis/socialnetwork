463a60772195d49bcad8c9dcbad53cfd563dae74
Josh Davis <josh@joshldavis.com>
2013-12-20 13:53:21 -0600

    Shortcircuit edit page

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,6 +5,6 @@ from profiles import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.profile, name='profile_self'),
-    url(r'^(?P<username>\w+)/$', views.profile, name='profile'),
     url(r'^edit/', views.edit, name='edit'),
+    url(r'^(?P<username>\w+)/$', views.profile, name='profile'),
 )
