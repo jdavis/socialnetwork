89afa09091f0ae515da92a445c7b4eb8adf51034
Josh Davis <josh@joshldavis.com>
2013-12-20 02:09:09 -0600

    Use post, not get for friends

--- friends/views.py
+++ friends/views.py
@@ -7,32 +7,38 @@ class FriendsView(generic.TemplateView):
 
     def get(self, request, *args, **kwargs):
         friend_list = get_friends(request.user)
-        if "addFriend" in request.GET:
-            # Determine if user exists.
-            toAddUsername = request.GET["addFriend"]
-            friend = None
-            try:
-                friend = User.objects.get(username=toAddUsername)
-            except User.DoesNotExist:
-                kwargs["error"] = "Couldn't find user %s" % toAddUsername
-
-            if friend:
-                # Determine if user is already a friend.
-                isAlreadyFriend = False
-                for friend in friend_list:
-                    if request.GET["addFriend"] == friend.username:
-                        isAlreadyFriend = True
-
-                if not isAlreadyFriend:
-                    f = Friendship(user1=request.user, user2=friend)
-                    f.save()
-                    friend_list.append(friend)
-                else:
-                    msg = "You're already friends with %s." % toAddUsername
-                    kwargs["error"] = msg
-
         kwargs["friends"] = friend_list
         return super(FriendsView, self).get(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        friend_list = get_friends(request.user)
+        kwargs["friends"] = friend_list
+
+        # Determine if user exists.
+        friend_name = request.POST["friend_name"]
+        friend = None
+        try:
+            friend = User.objects.get(username=friend_name)
+        except User.DoesNotExist:
+            kwargs["error"] = "Couldn't find user %s" % friend_name
+
+        if friend:
+            # Determine if user is already a friend.
+            isAlreadyFriend = False
+            for friend in friend_list:
+                if friend_name == friend.username:
+                    isAlreadyFriend = True
+
+            if not isAlreadyFriend:
+                f = Friendship(user1=request.user, user2=friend)
+                f.save()
+                friend_list.append(friend)
+            else:
+                msg = "You're already friends with %s." % friend_name
+                kwargs["error"] = msg
+
+        return super(FriendsView, self).get(self, request, *args, **kwargs)
+
 
 
 def get_friends(user):
